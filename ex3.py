dfa92acf2611f260b5cf9cd5e9d56d764387573c
Hao Peng <haopeng@wless-user-172019082015.temp.wsu.edu>
2019-01-17 14:44:01 -0800

    second commit

---
+++ ex3.py
@@ -0,0 +1,47 @@
+print("I will now count my chickens:")
+
+# 30 / 6 = 5, 5 + 25 = 30
+print("Hens", 25 + 30 / 6)
+
+# 25 * 3 = 75, the remaining of 75 / 4 is 3, 100 - 3 = 97
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+# 3 + 2 + 1 - 5 = 1, the remaining of 4 / 2 is 0, 1 / 4 = 0.25
+# 1 + 0 - 0.25 + 6 = 6.75
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+# >>> print(25 + 30 / 6)
+# 30.0
+# >>> print(100 - 25 * 3 % 4)
+# 97
+# >>> print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# 6.75
+# >>> print(3 + 2 < 5 - 7)
+# False
+# >>> print(3 + 2)
+# 5
+# >>> print(5 - 7)
+# -2
+# >>> print(5 > -2)
+# True
+# >>> print(5 >= -2)
+# True
+# >>> print(5 <= -2)
+# False
