fcbc61066575b4c2b311ce1620d57508f2ce3cf8
Hao Peng <haopeng@yings-MacBook-Pro.local>
2019-01-18 22:45:25 -0800

    ex13.py

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+# read the WYSS section for how to run this
+# the argv has four unpacked values
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
