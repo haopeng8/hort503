934d72b70477a3851528735b6a29760b760ffcf3
haopeng8 <hao_peng@wsu.edu>
2019-02-04 22:10:57 -0800

    ex38.py

--- Assignment05/ex38.py
+++ Assignment05/ex38.py
@@ -10,4 +10,14 @@ while len(stuff) != 10:
     next_one = more_stuff.pop()
     print("Adding: ", next_one)
     stuff.append(next_one)
-    
+    print(f"There are {len(stuff)} items now.")
+
+print("There we go: ", stuff)
+
+print("Let's do some things with stuff.")
+
+print(stuff[1])
+print(stuff[-1]) # whoa fancy
+print(stuff.pop())
+print(' '.join(stuff)) #what? cool!
+print('#'.join(stuff[3:5])) # super stellar!
